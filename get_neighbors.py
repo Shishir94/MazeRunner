99ac8c73ed36502381b6532b67c4c4240f03645d
Shishir Umesh <umesh.shishir@gmail.com>
2019-09-06 21:41:34 -0400

    Update get_neighbors.py
    Documented the code for the utility function getNeighbors

--- get_neighbors.py
+++ get_neighbors.py
@@ -1,25 +1,38 @@
-def traversable_neighbors(dfs_maze, node):
+def traversable_neighbors(maze, node):
+    """
+    Utility function to obtain all the traversable neighbors of the current cell
+
+    maze :- nxn matrix consisting the randomly generated maze.
+    node :- The cell who's traversable neighbors need ot be found
+    """
+
+    #Sets the current node and initializes an empty neighbors array
     (i,j) = node
     neighbors = []
-
+    #The outer "if" conditions check for all the boundary conditions
     if i-1 >= 0:
         up = (i-1,j)
-        if(dfs_maze[up] != 1 and dfs_maze[up] != -1):
+        #Checks if the cell is not a wall or already visited.
+        if(maze[up] != 1 and maze[up] != -1):
             neighbors.append(up)
 
     if j-1 >= 0:
         left = (i,j-1)
-        if(dfs_maze[left] != 1 and dfs_maze[left] != -1):
+        #Checks if the cell is not a wall or already visited.
+        if(maze[left] != 1 and maze[left] != -1):
             neighbors.append(left)
 
-    if i+1 < len(dfs_maze):
+    if i+1 < len(maze):
         bottom = (i+1,j)
-        if(dfs_maze[bottom] != 1 and dfs_maze[bottom] != -1):
+        #Checks if the cell is not a wall or already visited.
+        if(maze[bottom] != 1 and maze[bottom] != -1):
             neighbors.append(bottom)
 
-    if j+1 < len(dfs_maze):
+    if j+1 < len(maze):
         right = (i,j+1)
-        if(dfs_maze[right] != 1 and dfs_maze[right] != -1):
+        #Checks if the cell is not a wall or already visited.
+        if(maze[right] != 1 and maze[right] != -1):
             neighbors.append(right)
 
+    #Returns the list containing the traversable neighbors
     return neighbors
